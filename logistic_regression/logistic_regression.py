36cbc2baf1097b80923ee9bd44f35b7dd1d4bf1b
moonheart <fzp1205561571@hotmail.com>
2020-01-10 22:59:38 +0800

    logistic regression

---
+++ logistic_regression/logistic_regression.py
@@ -0,0 +1,221 @@
+import numpy as np
+import h5py
+import matplotlib.pyplot as plt
+
+
+def load_dataset():
+    train_dataset = h5py.File('datasets/train_catvnoncat.h5', 'r')
+    train_set_x_orig = np.array(train_dataset['train_set_x'][:])
+    train_set_y_orig = np.array(train_dataset['train_set_y'][:])
+
+    test_dataset = h5py.File('datasets/test_catvnoncat.h5', 'r')
+    test_set_x_orig = np.array(test_dataset['test_set_x'][:])
+    test_set_y_orig = np.array(test_dataset['test_set_y'][:])
+
+    classes = np.array(test_dataset['list_classes'][:])
+
+    train_set_y_orig = train_set_y_orig.reshape((1, train_set_y_orig.shape[0]))
+    test_set_y_orig = test_set_y_orig.reshape((1, test_set_y_orig.shape[0]))
+
+    train_set_x_flatten = train_set_x_orig.reshape(train_set_x_orig.shape[0], -1).T
+    test_set_x_flatten = test_set_x_orig.reshape(test_set_x_orig.shape[0], -1).T
+
+    train_set_x = train_set_x_flatten / 255
+    test_set_x = test_set_x_flatten / 255
+
+    return train_set_x, train_set_y_orig, test_set_x, test_set_y_orig, classes
+
+
+def sigmoid(z):
+    """
+    Compute the sigmoid of z
+
+    :param z: A scalar or numpy array of any size.
+
+    :return:
+         s -- sigmoid(z)
+    """
+    s = 1 / (1 + np.exp(-z))
+    return s
+
+
+def initialize_with_zeros(dim):
+    """
+    This function creates a vector of zeros of shape (dim, 1) for w and initializes b to 0.
+
+    :param dim: size of the w vector we want (or number of parameters in this case)
+
+    :return:
+        w -- initialized vector of shape (dim, 1)
+        b -- initialized scalar (corresponds to the bias)
+    """
+    w = np.zeros((dim, 1))
+    b = 0
+    return w, b
+
+
+def propagate(w, b, X, Y):
+    """
+   Implement the cost function and its gradient for the propagation explained above
+
+   :param w: weights, a numpy array of size (num_px * num_px * 3, 1)
+   :param b: bias, a scalar
+   :param X: data of size (num_px * num_px * 3, number of examples)
+   :param Y: true "label" vector (containing 0 if non-cat, 1 if cat) of size (1, number of examples)
+
+   :return:
+       cost -- negative log-likelihood cost for logistic regression
+       dw -- gradient of the loss with respect to w, thus same shape as w
+       db -- gradient of the loss with respect to b, thus same shape as b
+   """
+    m = X.shape[1]  # number of examples
+
+    # forward propagation
+    A = sigmoid(np.dot(w.T, X) + b)  # compute activation
+    cost = -1 / m * np.sum(Y * np.log(A) + (1 - Y) * np.log(1 - A))  # compute cost
+
+    # backward propagation
+    dw = 1 / m * np.dot(X, (A - Y).T)
+    db = 1 / m * np.sum(A - Y)
+
+    cost = np.squeeze(cost)
+
+    grads = {'dw': dw,
+             'db': db}
+
+    return grads, cost
+
+
+def optimize(w, b, X, Y, num_iterations, learning_rate, print_cost=False):
+    """
+    This function optimizes w and b by running a gradient descent algorithm
+
+    :param w:  weights, a numpy array of size (num_px * num_px * 3, 1)
+    :param b:  bias, a scalar
+    :param X:  data of shape (num_px * num_px * 3, number of examples)
+    :param Y:  true "label" vector (containing 0 if non-cat, 1 if cat), of shape (1, number of examples)
+    :param num_iterations:  number of iterations of the optimization loop
+    :param learning_rate:  learning rate of the gradient descent update rule
+    :param print_cost:  True to print the loss every 100 steps
+
+    :return:
+        params -- dictionary containing the weights w and bias b
+        grads -- dictionary containing the gradients of the weights and bias with respect to the cost function
+        costs -- list of all the costs computed during the optimization, this will be used to plot the learning curve.
+    """
+
+    costs = []
+
+    for i in range(num_iterations):
+        # Cost and gradient calculation
+        grads, cost = propagate(w, b, X, Y)
+
+        # Retrieve derivatives from grads
+        dw = grads["dw"]
+        db = grads["db"]
+
+        # update rule
+        w = w - learning_rate * dw
+        b = b - learning_rate * db
+
+        # Record the costs
+        if i % 100 == 0:
+            costs.append(cost)
+
+        # Print the cost every 100 training examples
+        if print_cost and i % 100 == 0:
+            print("Cost after iteration %i: %f" % (i, cost))
+
+    params = {"w": w,
+              "b": b}
+
+    grads = {"dw": dw,
+             "db": db}
+
+    return params, grads, costs
+
+
+def predict(w, b, X):
+    """
+    Predict whether the label is 0 or 1 using learned logistic regression parameters (w, b)
+
+    :param w: weights, a numpy array of size (num_px * num_px * 3, 1)
+    :param b: bias, a scalar
+    :param X: data of size (num_px * num_px * 3, number of examples)
+
+    :return:
+        Y_prediction -- a numpy array (vector) containing all predictions (0/1) for the examples in X
+    """
+    m = X.shape[1]  # number of examples
+    Y_prediction = np.zeros((1, m))
+
+    # Compute vector "A" predicting the probabilities of a cat being present in the picture
+    A = sigmoid(np.dot(w.T, X) + b)
+
+    for i in range(A.shape[1]):
+        # Convert probabilities A[0,i] to actual predictions p[0,i]
+        if A[0, i] <= 0.5:
+            Y_prediction[0, i] = 0
+        else:
+            Y_prediction[0, i] = 1
+
+    return Y_prediction
+
+
+def model(X_train, Y_train, X_test, Y_test, num_iterations=2000, learning_rate=0.5, print_cost=False):
+    """
+    Builds the logistic regression model by calling the function you've implemented previously
+
+    :param X_train: training set represented by a numpy array of shape (num_px * num_px * 3, m_train)
+    :param Y_train: training labels represented by a numpy array (vector) of shape (1, m_train)
+    :param X_test: test set represented by a numpy array of shape (num_px * num_px * 3, m_test)
+    :param Y_test: test labels represented by a numpy array (vector) of shape (1, m_test)
+    :param num_iterations: hyperparameter representing the number of iterations to optimize the parameters
+    :param learning_rate: hyperparameter representing the learning rate used in the update rule of optimize()
+    :param print_cost: Set to true to print the cost every 100 iterations
+
+    :return:
+        d -- dictionary containing information about the model.
+    """
+
+    # initialize parameters with zeros
+    w, b = initialize_with_zeros(X_train.shape[0])
+
+    # Gradient descent
+    parameters, grads, costs = optimize(w, b, X_train, Y_train, num_iterations, learning_rate, print_cost)
+
+    # Retrieve parameters w and b from dictionary "parameters"
+    w = parameters["w"]
+    b = parameters["b"]
+
+    # Predict test/train set examples
+    Y_prediction_test = predict(w, b, X_test)
+    Y_prediction_train = predict(w, b, X_train)
+
+    # Print train/test Errors
+    print("train accuracy: {} %".format(100 - np.mean(np.abs(Y_prediction_train - Y_train)) * 100))
+    print("test accuracy: {} %".format(100 - np.mean(np.abs(Y_prediction_test - Y_test)) * 100))
+
+    d = {"costs": costs,
+         "Y_prediction_test": Y_prediction_test,
+         "Y_prediction_train": Y_prediction_train,
+         "w": w,
+         "b": b,
+         "learning_rate": learning_rate,
+         "num_iterations": num_iterations}
+
+    return d
+
+
+if __name__ == "__main__":
+    train_set_x, train_set_y, test_set_x, test_set_y, classes = load_dataset()
+    d = model(train_set_x, train_set_y, test_set_x, test_set_y, num_iterations=10000, learning_rate=0.005,
+              print_cost=True)
+
+    # Plot learning curve (with costs)
+    costs = np.squeeze(d['costs'])
+    plt.plot(costs)
+    plt.ylabel('cost')
+    plt.xlabel('iterations (per hundreds)')
+    plt.title("Learning rate =" + str(d["learning_rate"]))
+    plt.show()
